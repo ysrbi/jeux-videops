1814b7a86c0b9318bd25f5bd29112caf625affba
Yousra Rouibi <yousra.rouibi@epitech.eu>
2026-04-17 00:20:16 +0200

    fix: affichage jeux github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
  
 export default defineConfig({
-  base: '/jeux-videops/',
+  base: './',
   plugins: [
     react(),
     viteStaticCopy({
